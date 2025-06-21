f0709702e565f5bc1430755d1a7c5ab2eeff9f39
Meow <seansean988@gmail.com>
2025-06-21 16:08:23 +0800

    server deployed version :

--- src/main/java/com/example/adkagents/AdkAgentsApplication.java
+++ src/main/java/com/example/adkagents/AdkAgentsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdkAgentsApplication {
     public static void main(String[] args) {
-        System.out.println("JWT_SECRET = " + System.getenv("JWT_SECRET"));
-        System.out.println("ENV FRONTEND: " + System.getenv("FRONTEND"));
+        
         SpringApplication.run(AdkAgentsApplication.class, args);
        
     }
